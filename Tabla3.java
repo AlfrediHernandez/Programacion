114e2dc802b0cddfca77d21c5995e26db30fd6fa
AlfrediHdz <alfredi1963@hotmail.com>
2020-09-08 09:14:41 -0500

    Se agrega tabla3 a Java1

---
+++ Tabla3.java
@@ -0,0 +1,16 @@
+public class Tabla3{
+	public static void main(String [] ar){
+		int tab=3,r;
+		System.out.println("-------------------------------------");
+		System.out.println("-            TABLA DEL 3            -");
+		System.out.println("-------------------------------------");
+		for(int a=1; a<=9; a++){
+			r = tab * a;
+			System.out.println(tab + " X " + a + " = "+r);
+		}
+		System.out.println("                                     ");
+		System.out.println("-------------------------------------");
+		System.out.println("-   Alfredi Hernandez 8-sep-2020    -");
+		System.out.println("-------------------------------------");
+	}
+}
