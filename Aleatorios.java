4b1b1a92eb60ac5e587a5a3a07156ddac8a88920
AlfrediHdz <alfredi1963@hotmail.com>
2020-09-13 12:11:50 -0500

    Se agrega Aleatorios a Java1

---
+++ Aleatorios.java
@@ -0,0 +1,11 @@
+public class Aleatorios{
+	public static void main(String [] ar){
+		int alea;
+		alea = (int)(Math.random()*1000);
+		System.out.print(alea);
+		System.out.println("                                      ");
+		System.out.println("--------------------------------------");
+		System.out.println("-    AlfrediHernandez 13-sep-2020    -");
+		System.out.println("--------------------------------------");
+	}
+}
