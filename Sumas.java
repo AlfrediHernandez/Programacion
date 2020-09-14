c53d0257ab5b877f821ebccb9f0ad8fccb01fd7f
AlfrediHdz <alfredi1963@hotmail.com>
2020-09-14 09:38:17 -0500

    Se agrega Sumas a Java1

---
+++ Sumas.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Sumas{
+	public static void main(String [] ar){
+		Scanner sc = new Scanner(System.in);
+		int a,b,r;
+		System.out.println("-------------------------------");
+		System.out.println("-            SUMANDO          -");
+		System.out.println("-------------------------------");
+		System.out.print("Ingrese a:	");
+		a = sc.nextInt();
+		System.out.print("Ingrese b:	");
+		b = sc.nextInt();
+		r = a + b;
+		System.out.println("             ----------");
+		System.out.println("Resultado:	"+r);
+		System.out.println("                                ");
+		System.out.println("--------------------------------");
+		System.out.println("- AlfrediHernandez 14/sep/2020 -");
+		System.out.println("--------------------------------");
+	}
+}
